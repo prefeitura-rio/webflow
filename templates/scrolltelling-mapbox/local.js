f27d843a3871c15f2a8f5d570b85a15dd124a6e4
joaoc <joao.carabetta@gmail.com>
2022-02-17 11:32:24 -0300

    first

---
+++ templates/scrolltelling-mapbox/local.js
@@ -0,0 +1,37 @@
+function createElement(tag, attrs, appendTo) {
+    var element = document.createElement(tag);
+    for (var attr in attrs) {
+        element.setAttribute(attr, attrs[attr]);
+    }
+    // insert element to DOM as last child
+    if (appendTo === undefined) {
+        document.body.appendChild(element);}
+    else {
+        appendTo.appendChild(element);}
+    return element;
+    
+}
+
+function createLocalRecords(config) {
+
+    console.log('Testing local records');
+
+    // create div as container for map
+    createElement('div', {id: 'map'});
+    var story = createElement('div', {id: 'story'});
+    createElement('div', {id: 'features'}, story);
+
+    // Cria container para cada chapter
+    config.chapters.forEach((record, idx) => {
+        var container = document.createElement('div');
+        container.setAttribute('id', record.id);
+
+        container.classList.add('section');
+        if (idx === 0) {
+            container.classList.add('active');
+        }
+
+        features.appendChild(container);
+    })
+};
+
