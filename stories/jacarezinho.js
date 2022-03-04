aca6f1ee33173d936954fe6efc85c1d54c75d28d
caiojacintho <caio.rodrigues.jacintho@gmail.com>
2022-03-04 18:58:46 -0300

    jacarezinho2

--- stories/jacarezinho.js
+++ stories/jacarezinho.js
@@ -80,7 +80,7 @@ var config = {
                 },
                 {
                     layer: 'jacarezinho-full',
-                    opacity: .2,
+                    opacity: .3,
                 }
             ],
             onChapterExit: [{
@@ -134,7 +134,7 @@ var config = {
                 },
                 {
                     layer: 'jacarezinho-full',
-                    opacity: .2,
+                    opacity: .3,
                 }
             ],
             onChapterExit: [{
