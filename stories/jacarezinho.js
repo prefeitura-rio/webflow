d0d3866b373ab1c3411bf67b390f0e7935281600
caiojacintho <caio.rodrigues.jacintho@gmail.com>
2022-03-04 11:37:06 -0300

    my-first-commit

--- stories/jacarezinho.js
+++ stories/jacarezinho.js
@@ -4,85 +4,82 @@ var config = {
     theme: 'light',
     use3dTerrain: true,
     chapters: [{
-      id: 'riodejaneiro',
-      // mapAnimation: 'easeTo', // flyTo, easeTo asd
-      location: {
-        mobile: {
-          center: {
-            lon: -43.41869,
-            lat: -22.95622
-          },
-          zoom: 10.85,
-          pitch: 27.00,
-          bearing: -2.79,
-          speed: 2,
-          curve: 1
-          },
-        desktop: {
-          center: {
-            lon: -43.41869,
-            lat: -22.95622
-          },
-          zoom: 8.85,
-          pitch: 27.00,
-          bearing: -2.79,
-          speed: 2,
-          curve: 1
-          }
-      },
-      rotateAnimation: {
-        duration: 100, // seconds
-        degrees: 180 // degress
-      },
-      mapAnimation: 'flyTo',
-      // callback: 'newContainer', 
-      onChapterEnter: [{
-        layer: 'jacarezinho-full',
-        opacity: 1,
-      }, ],
-      onChapterExit: [{
-        layer: 'jacarezinho-full',
-        opacity: 0,
-      }]
-    },
-    {
-      id: 'bairro',
-      location: {
-        mobile: {
-          center: {
-            lon: -43.25119,
-            lat: -22.88566
-          },
-          zoom: 14.98,
-          pitch: 8.50,
-          bearing: -34.70,
-          speed: 2,
-          curve: 1
-        },
-        desktop: {
-            center: {
-              lon: -43.25119,
-              lat: -22.88566
+            id: 'riodejaneiro',
+            // mapAnimation: 'easeTo', // flyTo, easeTo 
+            location: {
+                mobile: {
+                    center: {
+                        lon: -43.41869,
+                        lat: -22.95622
+                    },
+                    zoom: 10.85,
+                    pitch: 27.00,
+                    bearing: -2.79,
+                    speed: 2,
+                    curve: 1
+                },
+                desktop: {
+                    center: {
+                        lon: -43.41869,
+                        lat: -22.95622
+                    },
+                    zoom: 8.85,
+                    pitch: 27.00,
+                    bearing: -2.79,
+                    speed: 2,
+                    curve: 1
+                }
             },
-            zoom: 14.98,
-            pitch: 8.50,
-            bearing: -34.70,
-            speed: 2,
-            curve: 1
-          },
+            rotateAnimation: {
+                duration: 100, // seconds
+                degrees: 180 // degress
+            },
+            mapAnimation: 'flyTo',
+            // callback: 'newContainer', 
+            onChapterEnter: [{
+                layer: 'jacarezinho-full',
+                opacity: 1,
+            }, ],
+            onChapterExit: [{
+                layer: 'jacarezinho-full',
+                opacity: 0,
+            }]
         },
-      mapAnimation: 'flyTo',
-      onChapterEnter: [
-      {
-        layer: 'jacarezinho-full',
-        opacity: 1,
-      }
-      ],
-      onChapterExit: [
         {
-          layer: 'jacarezinho-full',
-          opacity: 0,
-        }
-      ]
-    }, ]
-  };
+            id: 'bairro',
+            location: {
+                mobile: {
+                    center: {
+                        lon: -43.25119,
+                        lat: -22.88566
+                    },
+                    zoom: 14.98,
+                    pitch: 8.50,
+                    bearing: -34.70,
+                    speed: 2,
+                    curve: 1
+                },
+                desktop: {
+                    center: {
+                        lon: -43.25119,
+                        lat: -22.88566
+                    },
+                    zoom: 14.98,
+                    pitch: 8.50,
+                    bearing: -34.70,
+                    speed: 2,
+                    curve: 1
+                },
+            },
+            mapAnimation: 'flyTo',
+            onChapterEnter: [{
+                layer: 'jacarezinho-full',
+                opacity: 1,
+            }],
+            onChapterExit: [{
+                layer: 'jacarezinho-full',
+                opacity: 0,
+            }]
+        },
+    ]
+};
